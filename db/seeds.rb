a26898a5db19f91d2b166077b8e0ef33a6a57cdf
Jerel Lillywhite <supelillywhite@gmail.com>
2018-01-13 11:45:31 -0700

    Edited seeds file to make sample posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do |meal|
+  Meal.create!(date: Date.today, description: "#{meal} is the most delicious meal ever!", quantity: 3, category: "Mexican", location_area: "St George", thumb_image: "https://b.zmtcdn.com/data/reviews_photos/82e/73d161c17d93ed8f758bde7b179db82e.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A", main_image: "http://img1.cookinglight.timeinc.net/sites/default/files/styles/400xvariable/public/image/2016/04/main/1605p138-mushroom-charred-corn-tacos-with-guacamole.jpg?itok=PfZe4ZUE", status: "available", location_address: "42 W 475 N Circle, St George Utah, 84770", karma: "5")
+end
+
+puts "20 meals created"

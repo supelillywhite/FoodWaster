1db0592039230e046f14ee899f59d66e361483c3
Jerel Lillywhite <supelillywhite@gmail.com>
2018-01-13 11:23:53 -0700

    put in field validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :first_name, :last_name, :username, :description, :prof_image, :prof_thumb, :food_one, :food_two, :food_three
 end
